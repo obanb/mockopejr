3b0ff3652c675fef00c54d995836d697c2d58452
obanb <ondrej.basista@apitree.cz>
2022-07-04 17:28:37 +0200

    server, router, tests..

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,42 +1,53 @@
-import { Delays, greeter } from '../src/main.js';
+import { getUrlPath, getQueryParams, getQueryParamsPairs } from '../src/main.js';
 
 describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
+  it("should test url path get from url string", () => {
+    expect(getUrlPath('http://localhost:8080/path1/path2')).toBe('/path1/path2');
+    expect(getUrlPath('http://localhost:8080/')).toBe('/');
+    expect(getUrlPath('http://localhost:8080')).toBe('/');
+    expect(getUrlPath('http://localhost:8080/path1?arg1=2')).toBe('/path1');
+  }),
+  it("should test url query params get from url path string", () => {
+    expect(getQueryParams('http://localhost:8080/path1?arg1=2')[0]).toBe("arg1=2");
+    expect(getQueryParams('http://localhost:8080/path1?arg1=2?arg2=3')[0]).toBe("arg1=2");
+    expect(getQueryParams('http://localhost:8080/path1?arg1=2?arg2=3')[1]).toBe("arg2=3");
+  }),
+  it("should test url query params convert to key/value pair map", () => {
+    const queries = [
+      ["arg1=2"],
+      ["arg1=2", "arg2=3"]
+    ]
+    expect(getQueryParamsPairs(queries[0])).toEqual({arg1:'2'});
+    expect(getQueryParamsPairs(queries[1])).toEqual({arg1:'2', arg2: '3'});
+  })
 
-  let timeoutSpy: jest.SpyInstance;
+  it("bb",() => {
+    function* questionGenerator(): Generator<string, unknown, string> {
+      let state = 'idle';
+      const t = setInterval(() => {
+          console.log(state)
+          if (state === 'run') {
+              console.log('bezim')
+          }
+      }, 1000)
+      while (true) {
+        console.log('true')
+        if (state === 'stop') {
+          clearInterval(t)
+          break
+        }
+        state = yield state
+      }
+      return
+    }
+    const test = questionGenerator()
+    test.next()
+    test.next('run')
 
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
+    setInterval(() => {
+      test.next('stop')
+    }, 1)
 
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
-
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
-  });
+    expect(1).toBe(1);
+  })
 });
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,86 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import * as http from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
+import * as url from 'url';
+
+const _HTTP_CONTENT_TYPE = 'application/json'
+const _HTTP_ENCODING = 'application/json'
+
+const httpGateKeeper = (req: IncomingMessage) => {
+  const ct = req.headers["content-type"]
+  if(!ct.includes(_HTTP_CONTENT_TYPE)){
+    throw new Error('Unsupported content type header.')
+  }
+
+  const a = req.headers["accept"]
+  if(!a.includes(_HTTP_ENCODING)){
+    throw new Error('Unsupported accept header.')
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+
+export const getUrlPath = (uri: string) => url.parse(uri).pathname
+
+export const getQueryParams = (uri: string) => {
+  const path = url.parse(uri).path
+  const results = path.match(/\?(?<query>.*)/);
+  const { groups: { query } } = results;
+  const params = query.match(/(?<param>\w+)=(?<value>\w+)/g);
+  return params
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
+export const getQueryParamsPairs = (params: string[]) => {
+  const pairs =  params.reduce((acc, next) => {
+    const [param, value] = next.split('=')
+    acc[param] = value
+    return acc
+  },{})
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+  return pairs;
+}
+
+
+const routeTable: Record<string, (req: IncomingMessage, res: ServerResponse) => unknown> = {
+  'get/info': ((_, res) => {
+      res.writeHead(200)
+      res.end('ok')
+  })
+}
+
+const createRouter = (routerTable: Record<string, (req: IncomingMessage, res: ServerResponse) => unknown>) => (req: IncomingMessage, res: ServerResponse) => {
+   const method = req.method.toLowerCase()
+   const routes = Object.keys(routerTable)
+
+   let found = false
+   for(const route of routes){
+       const path = getUrlPath(req.url)
+       const key = `${method}${path}`
+     console.log(key)
+       if(key === route){
+          found = true
+          routerTable[key](req, res)
+          break
+       }
+   }
+
+   if(!found){
+      res.writeHead(404)
+      res.end('url not found')
+   }
+}
+
+
+const requestListener = function (req: IncomingMessage, res: ServerResponse) {
+  try {
+    httpGateKeeper(req)
+    const router = createRouter(routeTable)
+    router(req, res)
+  }catch(e){
+    res.writeHead(400)
+    res.end(e.message)
+  }
+}
+
+export function main(){
+  const server = http.createServer(requestListener);
+  server.listen(8090)
 }
