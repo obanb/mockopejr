31d674e029bbf96b5cedc4b723de56adb9ba17e1
obanb <ondrej.basista@apitree.cz>
2023-04-28 17:48:35 +0200

    add

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
-import { server } from './server.js';
-import { routerTables } from './routerTables.js';
-import { plugableServer } from './plugableServer.js';
-import { charts } from './core/charts.js';
+
 import express from 'express';
 
-const { PROXY_PORT, APP_PORT = 3333, CHART_PORT } = process.env;
+const {APP_PORT = 3333} = process.env;
 
-const chartServer = plugableServer.new(
-  { port: Number(CHART_PORT), desc: 'chart' },
-  {},
-);
-
-const chartGroup = charts.group(chartServer);
 
 export const main = async () => {
   console.log('Logr started..');
@@ -19,20 +10,21 @@ export const main = async () => {
 
   const srvr = express();
 
-  const routes: Record<string, unknown> = srvr.routes
-
   srvr.use(express.json({limit: '50mb'}));
 
 
-  await charts.reload(chartGroup)();
+  // await charts.reload(chartGroup)();
+  //
+  // server.runServer(
+  //   Number(APP_PORT),
+  //   routerTables.appRouterTable(chartGroup),
+  //   'app',
+  // );
+  //
+  // server.runServer(Number(PROXY_PORT), routerTables.proxyRouterTable, 'proxy');
+  //
+  // chartServer.run();
 
-  server.runServer(
-    Number(APP_PORT),
-    routerTables.appRouterTable(chartGroup),
-    'app',
-  );
 
-  server.runServer(Number(PROXY_PORT), routerTables.proxyRouterTable, 'proxy');
-
-  chartServer.run();
+  srvr.listen(APP_PORT)
 };
