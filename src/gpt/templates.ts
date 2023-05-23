491ce842ac173d15d17b6a8d31a4b543a9d067fe
obanb <ondrej.basista@apitree.cz>
2023-05-23 18:04:33 +0200

    rozpra

--- src/gpt/templates.ts
+++ src/gpt/templates.ts
@@ -1,5 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai';
-import { CreateCompletionRequest } from 'openai/api';
+import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -16,18 +15,26 @@ const templateSettings: Record<string, CreateCompletionRequest> = {
     frequency_penalty: 0.5,
     presence_penalty: 0.0,
     stop: ["You:"],
-    prompt: ""
   }
 }
 
 
 
-const generateJSON = async(jsonPattern: unknown, cfg: Omit<CreateCompletionRequest, 'prompt'> | null = null) => {
+const generateJSON = async(jsonPattern: unknown,  cfg: Omit<CreateCompletionRequest, 'prompt'> | null = null) => {
   const defaultCfg = templateSettings.determenisticCompletion;
 
   const mergedCfg =  mergeObj(defaultCfg, cfg);
 
-  const prompt =
+  const prompt =  `
+    1. read this JSON structure pattern: ${JSON.stringify(jsonPattern)}
+    2. generate JSON with same structure, but randomize content of each field (except for the field names)
+    3. make each generated data similar to the original data, but not the same (e.g. {postalCode: 12345} -> {postalCode: 12255}, {street: Pražská} -> {street: Jateční})
+    4. try to keep same language of each field (e.g. {street: Pražská} -> {street: Pražská}, {street: 5th Avenue} -> {street: 7th Avenue})
+    5. use same data types (e.g. {postalCode: 12345} -> {postalCode: 12345}, {postalCode: "12345"} -> {postalCode: "12345"})
+    6 apply additional prompt: ${mergedCfg.prompt}
+    7. return only JSON object
+  `
+  mergedCfg.prompt = prompt;
 
   const completion = await openai.createCompletion(mergedCfg);
 
